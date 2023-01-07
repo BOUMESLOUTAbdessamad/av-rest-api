a9f375fc032ccf58f2b082d020ba68f21bed5efd
Abdessamad <abdessamadbm31@gmail.com>
2023-01-07 20:30:48 +0100

    Add frontend files

---
+++ frontend/safari-net/src/Components/Pages/Home.js
@@ -0,0 +1,13 @@
+import Container from '@mui/material/Container';
+
+function Home () {
+
+    return (
+        <Container maxWidth="lg" style={{minHeight: 1000}}>
+            <div>
+                Home Page
+            </div>
+        </Container>
+    )
+}
+export default Home;
---
+++ frontend/safari-net/src/StyleConstants/Colors.js
@@ -0,0 +1,14 @@
+
+ const Colors = {
+    dark: "#2f2f2fff",
+    white: "#ffffffff",
+    oceanblue : "#2b65ec",
+    orange: "#f75631ff",
+    green: "#45a341",
+    skyblue: "#87ceeb",
+    cloud: "#f6f6f6",
+    earth: "#aa8b56"
+}
+
+
+export default Colors;
